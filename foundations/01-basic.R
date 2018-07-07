f28b3156f368fb9899a0f92046549c425b317e7a
anchu <chuvanan.cva@gmail.com>
2018-07-07 11:53:18 +0700

    Finalize section Foundations

--- foundations/01-basic.R
+++ foundations/01-basic.R
@@ -137,4 +137,21 @@ library(dplyr)
 
 ## Make sure you have the following packages installed on your laptop
 
-## readr, tidyr, dplyr, stringr, ggplot2
+## readr, tidyr, dplyr, stringr, ggplot2, knitr, rmarkdown
+
+## -----------------------------------------------------------------------------
+## R packages growth
+
+library(ggplot2)
+date <- c( "2017-12-15", "2017-07-04", "2017-01-27", "2016-08-22", "2016-02-29",
+          "2015-08-12", "2014-10-29", "2013-11-08", "2012-08-23", "2011-05-12",
+          "2009-10-04", "2007-04-12", "2004-10-01", "2003-04-01", "2002-09-17",
+          "1997-04-23")
+n <- c( 12000, 11000, 10000, 9000, 8000, 7000, 6000, 5000, 4000, 3000, 2000,
+       1000, 500, 250, 68, 12 )
+r_pkgs <- data.frame(date, n, stringsAsFactors = FALSE)
+r_pkgs$date <- as.Date(r_pkgs$date)
+ggplot(r_pkgs, aes(date, n)) +
+    geom_line() +
+    geom_point() +
+    labs(x = NULL, y = "Number of packages", title = "CRAN's package growth")
--- foundations/foundations.R
+++ foundations/foundations.R
@@ -1,7 +1,8 @@
 
 
 
-# R as calculator ---------------------------------------------------------
+## -----------------------------------------------------------------------------
+## Arithmetic with R
 
 
 12 + 22    # addition
@@ -19,22 +20,67 @@
 5 %/% 2    # integer division
 
 
-# R basics: Comment -------------------------------------------------------
+## Your turn ------------------------------
+
+## Type 2^5 in the editor to calculate 2 to the power 5.
 
 
-# print("this is the first comment line")
+## Type 28 %% 6 to calculate 28 modulo 6.
+
+
+## -----------------------------------------------------------------------------
+## Comments
+
+## Note how the symbol '#' is used to add comment on the R code
+
+## print("this is the first comment line")
 print("this is the second comment line")
 
 
+## -----------------------------------------------------------------------------
+## Variable Assignment
 
-# Assignment statements ---------------------------------------------------
+## A basic concept in (statistical) programming is called a 'variable'.
 
+## A variable allows you to store a value (e.g. 4) in R
+
+## Examples:
 celsius <- 24
 fahrenheit <- (celsius * 9 / 5) + 32
 print(fahrenheit)
 
 
-# Built-in functions ------------------------------------------------------
+## Your turn ------------------------------
+
+## Assign the value 42 to x
+
+
+## Print out the value of the variable x
+
+
+## Assign the value 5 to the variable 'my_apples'
+
+
+## Print out the value of the variable 'my_apples'
+
+
+## Assign to 'my_oranges' the value 6.
+
+
+## Add the variables 'my_apples' and 'my_oranges' and have R simply print the
+## result.
+
+
+## Assign the result of adding 'my_apples' and 'my_oranges' to a new variable
+## 'my_fruit'.
+
+
+
+## -----------------------------------------------------------------------------
+## Functions
+
+
+## Built-in functions
 
 abs(-145)    # absolute value
 
@@ -46,9 +92,7 @@ lm(dist ~ speed, data = cars)    # fit linear models
 
 plot(cars$speed, cars$dist)    # generate X-Y plot
 
-
-# User-written functions --------------------------------------------------
-
+## User-written functions
 
 say_hello <- function(to) {
     paste0("Hello ", to, ". Nice to meet you!")
@@ -57,25 +101,39 @@ say_hello <- function(to) {
 say_hello(to = "An Chu")
 
 
-# R Packages Growth -------------------------------------------------------
+## Your turn ------------------------------
 
-library(ggplot2)
-date <- c( "2017-12-15", "2017-07-04", "2017-01-27", "2016-08-22", "2016-02-29",
-           "2015-08-12", "2014-10-29", "2013-11-08", "2012-08-23", "2011-05-12",
-           "2009-10-04", "2007-04-12", "2004-10-01", "2003-04-01", "2002-09-17",
-           "1997-04-23")
-n <- c( 12000, 11000, 10000, 9000, 8000, 7000, 6000, 5000, 4000, 3000, 2000,
-        1000, 500, 250, 68, 12 )
-r_pkgs <- data.frame(date, n, stringsAsFactors = FALSE)
-r_pkgs$date <- as.Date(r_pkgs$date)
-ggplot(r_pkgs, aes(date, n)) +
-    geom_line() +
-    geom_point() +
-    labs(x = NULL, y = "Number of packages", title = "CRAN's package growth")
+## Write your own function to convert Fahrenheit to Celsius degree.
+
+to_celcius <- function(x) {
+    ## your code goes here
+
+}
+
+## Expected output:
+## to_celcius(x = 50)
+## R> [1] "50 Fahrenheit is equal to 10 Celcius"
 
 
-# Load packages -----------------------------------------------------------
+## -----------------------------------------------------------------------------
+## Packages
 
+
+## syntax:
+
+## install.packages(pkgs = "name_of_package(s)")
+
+install.packages("dplyr")
+install.packages(c("tidyr", "ggplot2"))
+
+## update packages
+update.packages()
+
+## Load package into R workspace
 library(dplyr)
 
+## Your turn ------------------------------
 
+## Make sure you have the following packages installed on your laptop
+
+## readr, tidyr, dplyr, stringr, ggplot2, knitr, rmarkdown
