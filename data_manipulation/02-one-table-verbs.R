a300b67d46b970eb793323283d7a61463e46b60d
anchu <chuvanan.cva@gmail.com>
2018-07-14 19:58:59 +0700

    Add solution to Q1

--- data_manipulation/02-one-table-verbs.R
+++ data_manipulation/02-one-table-verbs.R
@@ -19,6 +19,25 @@ filter(air_quality, aqi > mean(aqi, na.rm = TRUE))
 
 # select() ----------------------------------------------------------------
 
+# Q1 ----------------------------------------------------------------------
+
+air_quality %>%
+    group_by(year, month) %>%
+    summarise(mean = mean(aqi, na.rm = TRUE))
+
+air_quality %>%
+    filter(year == 2017) %>%
+    group_by(month) %>%
+    summarise(avg_aqi = mean(aqi, na.rm = TRUE))
+
+weather %>%
+    mutate(month = as.character(lubridate::month(date)),
+           year = lubridate::year(date)) %>%
+    filter(year == 2017) %>%
+    group_by(month) %>%
+    summarise(avg_temp = mean(max_temp, na.rm = TRUE))
+
+# ---------------------------------------
 
 select(air_quality, datetime:hour)
 
@@ -40,9 +59,10 @@ arrange(air_quality, desc(aqi))
 
 # mutate() ----------------------------------------------------------------
 
-mutate(air_quality, is_good = aqi <= 50)
+x <- mutate(air_quality, is_good = aqi <= 50)
 
-mutate(air_quality,
+
+air_quality <- mutate(air_quality,
        aqi_color_code = case_when(
            aqi <= 50 ~ "green",
            aqi >= 51 & aqi <= 100 ~ "yellow",
@@ -51,6 +71,23 @@ mutate(air_quality,
            aqi >= 201 & aqi <= 300 ~ "purple",
            aqi >= 301 ~ "brown"
        ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+View(x)
 
 air_quality %>%
     summarise(avg_aqi = mean(aqi, na.rm = TRUE))
