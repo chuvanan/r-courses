fc75460c2be02682eddd8969f34885d627ca3969
anchu <chuvanan.cva@gmail.com>
2018-07-15 07:20:27 +0700

    Update data sets

--- data/data_tidying/data_tidying.R
+++ data/data_tidying/data_tidying.R
@@ -5,14 +5,13 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 
-# install.packages("ggmap")
+## install.packages("ggmap")
 library(ggmap)
 
 
+## diem thi ----------------------------------------------------------------
 
-# diem thi ----------------------------------------------------------------
-
-diemthi <- read_csv("slides/data_tidying/diemthi_tslop10.csv")
+diemthi <- read_csv("data_tidying/diemthi_tslop10.csv")
 
 diemthi
 
@@ -21,14 +20,17 @@ diemthi_tidy <- gather(diemthi, key = "mon", value = "diem",
 diemthi_tidy
 
 
-# your turn: forest_fires.csv
+## your turn: forest_fires.csv ----------
 
 
 
+## your turn: race.csv ----------
 
-# airport gps -------------------------------------------------------------
 
-airport_gps <- read_csv("slides/data_tidying/airports_gps.csv")
+
+## airport gps -------------------------------------------------------------
+
+airport_gps <- read_csv("data_tidying/airports_gps.csv")
 
 airport_gps
 
@@ -39,7 +41,7 @@ airport_gps_tidy <- separate(airport_gps,
 
 airport_gps_tidy
 
-# bonus
+## bonus
 
 airport_gps_tidy$lon <- as.double(airport_gps_tidy$lon)
 airport_gps_tidy$lat <- as.double(airport_gps_tidy$lat)
@@ -51,14 +53,14 @@ ggmap(vnm) +
                aes(lon, lat),
                color = "brown", size = 3)
 
-# your turn: oecd_teacher_salary.csv
+## your turn: oecd_teacher_salary.csv ----------
 
 
 
 
-# toy example 3 -----------------------------------------------------------
+## toy example 3 -----------------------------------------------------------
 
-sales_kpi <- read_csv("slides/data_tidying/sales_kpi.csv")
+sales_kpi <- read_csv("data_tidying/sales_kpi.csv")
 
 sales_kpi <- gather(sales_kpi, key = "quater", value = "amount", q1:q4)
 
@@ -71,23 +73,33 @@ sales_kpi <- mutate(sales_kpi, quater = gsub("q", "", quater))
 
 sales_kpi
 
-# your turn: hanoi_temp_wideform.csv
+
+## your turn: cars_sales.csv ----------
 
 
 
-# final challenge ---------------------------------------------------------
+## your turn: hanoi_temp_wideform.csv ----------
 
-dat <- read_csv("slides/data_tidying/so3.csv")
+
+
+## your turn: grades.csv ----------
+
+
+
+## your turn: activities.csv ----------
+
+
+
+## final challenge ---------------------------------------------------------
+
+dat <- read_csv("data_tidying/so3.csv")
 dat
 
-# expected output:
+## expected output:
 
-#   Person Pre.Score1 Pre.Score2 Pre.Score3  Post.Score1 Post.Score2 Post.Score3
-# 1   greg         80         78       84.0           79          80        84.5
-# 2  sally         75         74       79.5           78          78        83.0
-# 3    sue         81         78       84.5           82          81        86.5
+##   Person Pre.Score1 Pre.Score2 Pre.Score3  Post.Score1 Post.Score2 Post.Score3
+## 1   greg         80         78       84.0           79          80        84.5
+## 2  sally         75         74       79.5           78          78        83.0
+## 3    sue         81         78       84.5           82          81        86.5
 
-# your solution goes here:
-
-
-
+## your solution goes here:
