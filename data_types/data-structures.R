47aa6f0eceb54308ce59d894d4cd8e51c2a82834
AnChu <chuvanan.cva@gmail.com>
2018-07-29 00:14:06 +0700

    update slide

--- data_types/data-structures.R
+++ data_types/data-structures.R
@@ -1,23 +1,57 @@
 
 
-
 library(forcats)
+library(ggplot2)
+library(dplyr)
+setwd("~/Documents/r-courses/data_types/")
+
+air_quality <- read.csv("air-quality.csv")
+diemthi <- readr::read_csv("THPT 2018 Quoc gia.csv")
+str(air_quality$aqi_categ)
+levels(air_quality$aqi_categ)
+
+
+# Change level order ------------------------------------------------------
+
+by_categ <-  filter(air_quality, !is.na(aqi_categ))
+by_categ <- mutate(by_categ,
+                   aqi_categ = fct_rev(fct_infreq(aqi_categ)))
+
+ggplot(by_categ, aes(aqi_categ)) +
+    geom_bar()
+
+# Change factor level -----------------------------------------------------
+
+by_categ <-  filter(air_quality, !is.na(aqi_categ))
+
+by_categ <- mutate(by_categ,
+                   aqi_categ = fct_collapse(aqi_categ,
+                        Acceptalbe = c("Good", "Moderate"),
+                        Dangerous = c("Unhealthy", "Unhealthy for Sensitive Groups"),
+                        Deadly = c("Very Unhealthy", "Hazardous")
+                   ))
+
+by_categ$aqi_categ <- fct_relevel(by_categ$aqi_categ, "Acceptalbe", "Dangerous")
+
+ggplot(by_categ, aes(aqi_categ)) +
+    geom_bar()
+
+
+# Patterned vectors -------------------------------------------------------
+
+
+# Exercise 1
+
+m <- 1:12
+m <- ifelse(nchar(m) == 1, paste0("0", m), m)
+y <- 2012:2018
+
+paste0(
+    rep(m, times = length(y)), "/",
+    rep(y, each = 12)
+    )
 
 
 
-str(gss_cat$race)
-levels(gss_cat$race)
-
-count(gss_cat, race)
-
-with(gss_cat, table(race))
 
 
-ggplot(gss_cat, aes(race)) +
-    geom_bar() +
-    scale_x_discrete(drop = FALSE)
-
-
-gss_cat %>%
-    group_by(race) %>%
-    summarise(n = n())
